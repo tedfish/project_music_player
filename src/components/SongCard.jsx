8ddd4d72b5e0373550fbd041f2cbaea01055ea01
tedfish <tedfish@gmail.com>
2024-02-23 12:43:07 -0800

    keys

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -18,7 +18,7 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
   };
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="flex flex-col w-[250px] p-5 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
