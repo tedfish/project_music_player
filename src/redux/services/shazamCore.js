a3af5a3d2b54baeda00216fc3fa8d6a92a641203
tedfish <tedfish@gmail.com>
2024-02-23 12:06:05 -0800

    demo

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,23 +1,40 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const shazamCoreApi = createApi({
-  reducerPath: 'shazamCoreApi',
+  reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/',
+    baseUrl: "https://shazam-core.p.rapidapi.com/",
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+      headers.set(
+        "X-RapidAPI-Key",
+        "a8f9a07df4msh9eb6f146ccedce3p18df4ajsn7190d1ac9550"
+        // import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+      );
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
-    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
-    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
-    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
-    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
-    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
-    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
+    }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) =>
+        `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
+    }),
   }),
 });
 
@@ -30,3 +47,16 @@ export const {
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
 } = shazamCoreApi;
+
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+//     'X-RapidAPI-Key': 'a8f9a07df4msh9eb6f146ccedce3p18df4ajsn7190d1ac9550'
+//   }
+// };
+
+// fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
